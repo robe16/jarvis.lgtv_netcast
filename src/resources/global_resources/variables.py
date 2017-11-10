4c7a294028923d4086eae3dff8c8630a97bf82dc
Rob <robe16@hotmail.co.uk>
2017-11-10 22:42:06 +0000

    Addition of HTTP response codes to variables.py

--- src/resources/global_resources/variables.py
+++ src/resources/global_resources/variables.py
@@ -28,3 +28,10 @@ timeformat = '%Y/%m/%d %H.%M.%S.%f'
 uri_info = '/info/<resource_requested>'
 uri_command = '/command'
 uri_image = '/img/<filename>'
+
+httpStatusSuccess = 200
+httpStatusBadrequest = 400
+httpStatusForbidden = 404
+httpStatusFailure = 420
+httpStatusServererror = 500
+
