4617fb1f029ec516bc4067dcee7bed5c645bdf75
Rob <robe16@hotmail.co.uk>
2017-11-10 14:26:13 +0000

    Initial commit of global variables and resources.

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM resin/rpi-raspbian:latest
+MAINTAINER robe16
+
+# Port number to listen on
+ARG portApplication
+
+# Update
+RUN apt-get update && apt-get install -y python python-pip
+
+WORKDIR /jarvis/lgtv_netcast
+
+# Bundle app source
+COPY src /jarvis/lgtv_netcast
+
+# Copy app dependencies
+COPY requirements.txt requirements.txt
+
+# Install app dependencies
+RUN pip install -r requirements.txt
+
+# Expose the application port and run application
+EXPOSE ${portApplication}
+CMD python start.py ${portApplication}
---
+++ src/resources/global_resources/variables.py
@@ -0,0 +1,24 @@
+serviceName = 'Jarvis: LGTV (Netcast OS)'
+deviceName = 'lgtv_netcast'
+
+logFileName = 'lgtv_netcast'
+logFileNameTimeformat = '%Y-%m-%d'
+
+logLevelUnset = 'unset'
+logLevelDebug = 'debug'
+logLevelInfo = 'info'
+logLevelWarning = 'warning'
+logLevelError = 'error'
+logLevelCritical = 'critical'
+
+logDesc = ''
+logDescDevicePairing = 'Device pairing'
+logDescDeviceShowpairkey = 'Show pairing key'
+logDescDeviceGetapplist = 'Get app list'
+logDescDeviceGetappicon = 'Get app icon'
+logDescDeviceGetcurrentchannel = 'Get current channel'
+logDescDeviceExecuteapp = 'Execute app'
+logDescDeviceSendcommand = 'Send command'
+
+timeformat = '%Y/%m/%d %H.%M.%S.%f'
+
