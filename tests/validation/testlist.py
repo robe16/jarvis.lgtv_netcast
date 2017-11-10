44ec840abbb4392ac2664a6e97639246a846803a
Rob <robe16@hotmail.co.uk>
2017-11-10 16:32:42 +0000

    Initial commit of schema validation & tests

---
+++ tests/validation/testlist.py
@@ -0,0 +1,18 @@
+testlist = [
+    {"id": 1,
+     "fileName": "command.test1",
+     "schema": "command",
+     "expectedResult": True},
+    {"id": 2,
+     "fileName": "command.test2",
+     "schema": "command",
+     "expectedResult": True},
+    {"id": 3,
+     "fileName": "command.test3",
+     "schema": "command",
+     "expectedResult": False},
+    {"id": 4,
+     "fileName": "command.test4",
+     "schema": "command",
+     "expectedResult": False}
+]
