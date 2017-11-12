593579f06323bbbbbf5e822ae508b05620210e95
Rob <robe16@hotmail.co.uk>
2017-11-12 09:40:29 +0000

    Discovery now limited to 20 second period. If not recieved within that time, False is returned.

--- src/config/discovery.py
+++ src/config/discovery.py
@@ -1,13 +1,18 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from resources.global_resources.variables import server_broadcastPort, server_broadcastCode
+import datetime
 
 
 def discover_server():
     s = socket(AF_INET, SOCK_DGRAM) # create UDP socket
     s.bind(('', server_broadcastPort))
 
-    while True:
+    start_time = datetime.datetime.now()
+
+    while start_time > (datetime.datetime.now() + datetime.timedelta(seconds = 20)):
         data, addr = s.recvfrom(1024) # wait for a packet
         data = data.decode("utf-8")
         if data.startswith(server_broadcastCode):
             return data[len(server_broadcastCode):]
+
+    return False
