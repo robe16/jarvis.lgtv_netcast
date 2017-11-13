56a5d668841eea2b55f2a37b70908908cb81e972
Rob <robe16@hotmail.co.uk>
2017-11-13 00:03:49 +0000

    Attempt to fix bug - container fails to start successfully

--- src/run.py
+++ src/run.py
@@ -17,6 +17,10 @@ try:
     ################################
     # Receive sys arguments
 
+    print("This is the name of the script: ", sys.argv[0])
+    print("Number of arguments: ", len(sys.argv))
+    print("The arguments are: ", str(sys.argv))
+
     # Argument 1: Port of self exposed on host
     try:
         self_hostport = sys.argv[1]
