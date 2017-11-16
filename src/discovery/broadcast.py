748595e426ff1f76188d611525a41728c6e628c7
Rob <robe16@hotmail.co.uk>
2017-11-16 22:11:19 +0000

    Broadcast did not send out to local network - attempted fix.

--- src/discovery/broadcast.py
+++ src/discovery/broadcast.py
@@ -6,7 +6,7 @@ from resources.global_resources.variables import serviceType, jarvis_broadcastPo
 
 def broadcast_service(service_id, host_port):
     s = socket(AF_INET, SOCK_DGRAM)  # create UDP socket
-    s.bind(('', 0))
+    s.bind(('0.0.0.0', 0))
     s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # this is a broadcast socket
 
     msg = jarvis_broadcast_msg.format(service_id=service_id,
@@ -16,3 +16,5 @@ def broadcast_service(service_id, host_port):
     while True:
         s.sendto(msg, ('<broadcast>', jarvis_broadcastPort))
         sleep(broadcast_frequency)
+
+broadcast_service('test', '0000')
