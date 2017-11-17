6ef1eed257c1824e04a632f7b4e82e61f75b05e4
Rob <robe16@hotmail.co.uk>
2017-11-17 23:12:07 +0000

    Update to convert port value to integer and add exception handling.

--- src/discovery/broadcast.py
+++ src/discovery/broadcast.py
@@ -1,18 +1,26 @@
 from time import sleep
 from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST
 from parameters import broadcast_frequency
-from resources.global_resources.variables import serviceType, jarvis_broadcastPort, jarvis_broadcast_msg
+from resources.global_resources.variables import *
+from log.log import Log
+
+
+_log = Log()
 
 
 def broadcast_service(service_id, self_port, self_broadcastPort):
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.bind(('0.0.0.0', self_broadcastPort))
-    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-
-    msg = jarvis_broadcast_msg.format(service_id=service_id,
-                                      service_type=serviceType,
-                                      port=str(self_port))
-
-    while True:
-        s.sendto(msg, ('<broadcast>', jarvis_broadcastPort))
-        sleep(broadcast_frequency)
+    try:
+        s = socket(AF_INET, SOCK_DGRAM)
+        s.bind(('0.0.0.0', int(self_broadcastPort)))
+        s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+        #
+        msg = jarvis_broadcast_msg.format(service_id=service_id,
+                                          service_type=serviceType,
+                                          port=str(self_port))
+        #
+        while True:
+            s.sendto(msg, ('<broadcast>', jarvis_broadcastPort))
+            sleep(broadcast_frequency)
+        #
+    except Exception as e:
+        _log.new_entry(logCategoryProcess, '-', 'Broadcasting service', e, 'fail', level=logLevelError)
