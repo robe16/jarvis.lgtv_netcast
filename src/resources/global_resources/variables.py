777d441c36066fba5ba57aac96e529d8daa737e7
Rob <robe16@hotmail.co.uk>
2017-12-08 23:00:23 +0000

    variables split into independant python files for logging and broadcast values.

--- src/resources/global_resources/variables.py
+++ src/resources/global_resources/variables.py
@@ -1,20 +1,6 @@
 serviceName = 'Jarvis: LGTV (Netcast OS)'
 serviceType = 'tv_lg_netcast'
 
-logFileNameTimeformat = '%Y-%m-%d'
-
-# NOTE: delimiter-separated value in log files is '::'
-logMsg_Inbound_Info = ':{timestamp}::{serviceid}::{servicetype}::INBOUND::{result}::{client}::{uri}::{method}::{httpresponse}::{desc}'
-logMsg_Inbound_Error = ':{timestamp}::{serviceid}::{servicetype}::INBOUND::{result}::{exception}::{client}::{uri}::{method}::{httpresponse}::{desc}'
-logMsg_Internal_Info = ':{timestamp}::{serviceid}::{servicetype}::INTERNAL::{result}::{operation}::{desc}'
-logMsg_Internal_Error = ':{timestamp}::{serviceid}::{servicetype}::INTERNAL::{result}::{exception}::{operation}::{desc}'
-logMsg_Outbound_Info = ':{timestamp}::{serviceid}::{servicetype}::OUTBOUND::{result}::{ip}::{uri}::{method}::{httpresponse}::{desc}'
-logMsg_Outbound_Error = ':{timestamp}::{serviceid}::{servicetype}::OUTBOUND::{result}::{exception}::{ip}::{uri}::{method}::{httpresponse}::{desc}'
-
-logPass = 'PASS'
-logFail = 'FAIL'
-logException = 'EXCEPTION'
-
 logDescDevicePairing = 'Device pairing'
 logDescDeviceShowpairkey = 'Show pairing key'
 logDescDeviceGetapplist = 'Get app list'
@@ -22,8 +8,6 @@ logDescDeviceGetappicon = 'Get app icon'
 logDescDeviceGetcurrentchannel = 'Get current channel'
 logDescDeviceExecuteapp = 'Execute app'
 logDescDeviceSendcommand = 'Send command'
-
-timeformat = '%Y/%m/%d %H.%M.%S.%f'
 
 uri_config = '/config'
 uri_commands = '/commands'
